827a4a8139fa5b3bb2698c0bbbad74e07f485655
Tatia Chichinadze <echichinadze@credo.ge>
2024-10-27 19:18:58 +0400

    Add project files.

---
+++ While Loop/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int Number = 0;
+int Sum = 0;
+string EnteredNumber = "";
+
+while (Number >= 0)
+{
+    Console.WriteLine("Enter Number");
+    Number = int.Parse(Console.ReadLine());
+    if (Number > 0) 
+    {
+
+        Sum = Sum + Number;
+        
+    }
+
+    EnteredNumber += Number + ";";
+
+}
+   
+Console.WriteLine("Enter Numbers: " + EnteredNumber);
+Console.WriteLine("Sum:" + Sum);
